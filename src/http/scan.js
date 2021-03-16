861f1c6ac6d355aeccee8be14e961ea60f20df18
Julien Bouquillon <julien.bouquillon@sg.social.gouv.fr>
2021-03-16 22:03:50 +0100

    fixes

---
+++ src/http/scan.js
@@ -0,0 +1,69 @@
+const fs = require("fs");
+const path = require("path");
+const pAll = require("p-all");
+const fetch = require("node-fetch");
+
+const getUrls = () =>
+  fs
+    .readFileSync(path.join(__dirname, "..", "..", "urls.txt"))
+    .toString()
+    .split("\n")
+    .map((url) => url.replace(/^https?:\/\//, "".replace(/\/$/, ""))) // make hostnames
+    .filter(Boolean);
+
+const scan = () => {
+  const urls = getUrls();
+  return pAll(
+    urls.map((url) => async () => ({
+      url,
+      result: await scanHTTP(url),
+    })),
+    { concurrency: 1 }
+  );
+};
+
+const INTERVAL = 10000;
+const MAX_TRIES = 10;
+
+const API_HTTP = `https://http-observatory.security.mozilla.org/api/v1`;
+
+const scanHTTP = (url, tries = MAX_TRIES) => {
+  console.warn(`fetch mozilla HTTP API for ${url}`);
+  return fetch(`${API_HTTP}/analyze?host=${url}&hidden=true&rescan=true`, {
+    method: "POST",
+  })
+    .then((r) => r.json())
+    .then((result) => {
+      if (result.error) {
+        throw new Error(result.error);
+      }
+      if (result.state === "FINISHED") {
+        return fetch(`${API_HTTP}/getScanResults?scan=${result.scan_id}`, {
+          method: "GET",
+        })
+          .then((r) => r.json())
+          .then((details) => ({
+            ...result,
+            details,
+          }));
+      } else if (tries > 0) {
+        console.warn(
+          `delay HTTP ${url} (${MAX_TRIES - tries + 1}/${MAX_TRIES})`
+        );
+        return new Promise((resolve) =>
+          setTimeout(() => scanHTTP(url, tries - 1).then(resolve), INTERVAL)
+        );
+      }
+      throw new Error(`${MAX_TRIES} failed scans for ${url}`);
+    });
+};
+
+if (require.main === module) {
+  scan()
+    .then((results) => {
+      console.log(JSON.stringify(results, null, 2));
+    })
+    .catch(console.log);
+}
+
+module.exports = scan;
--- src/ssl/scan.js
+++ src/ssl/scan.js
@@ -16,8 +16,7 @@ const scan = () => {
   return pAll(
     urls.map((url) => async () => ({
       url,
-      tls: await scanTLS(url),
-      http: await scanHTTP(url),
+      result: await scanTLS(url),
     })),
     { concurrency: 1 }
   );
@@ -26,7 +25,6 @@ const scan = () => {
 const INTERVAL = 10000;
 const MAX_TRIES = 10;
 
-const API_HTTP = `https://http-observatory.security.mozilla.org/api/v1`;
 const API_TLS = `https://api.ssllabs.com/api/v2`;
 
 const scanTLS = async (url, tries = MAX_TRIES) => {
@@ -53,45 +51,16 @@ const scanTLS = async (url, tries = MAX_TRIES) => {
       ) {
         console.warn(
           `delay ${url} (${MAX_TRIES - tries + 1}/${MAX_TRIES}) (${
-            json.endpoints[0].progress
-          }%)`
+            json.endpoints[0].progress > -1
+              ? json.endpoints[0].progress + "%"
+              : "-"
+          })`
         );
         return new Promise((resolve) =>
           setTimeout(() => scanTLS(url, tries - 1).then(resolve), INTERVAL)
         );
       }
       return json;
-    });
-};
-
-const scanHTTP = (url, tries = MAX_TRIES) => {
-  console.warn(`fetch mozilla HTTP API for ${url}`);
-  return fetch(`${API_HTTP}/analyze?host=${url}&hidden=true&rescan=true`, {
-    method: "POST",
-  })
-    .then((r) => r.json())
-    .then((result) => {
-      if (result.error) {
-        throw new Error(result.error);
-      }
-      if (result.state === "FINISHED") {
-        return fetch(`${API_HTTP}/getScanResults?scan=${result.scan_id}`, {
-          method: "GET",
-        })
-          .then((r) => r.json())
-          .then((details) => ({
-            ...result,
-            details,
-          }));
-      } else if (tries > 0) {
-        console.warn(
-          `delay HTTP ${url} (${MAX_TRIES - tries + 1}/${MAX_TRIES})`
-        );
-        return new Promise((resolve) =>
-          setTimeout(() => scanHTTP(url, tries - 1).then(resolve), INTERVAL)
-        );
-      }
-      throw new Error(`${MAX_TRIES} failed scans for ${url}`);
     });
 };
 
